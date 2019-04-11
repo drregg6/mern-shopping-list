a3f347f1432f1790e627ca6268bc34a01643bfc4
Dave <drregg6@gmail.com>
2019-04-10 20:29:16 -0400

    scripts ready to run concurrently

--- routes/api/items.js
+++ routes/api/items.js
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
 });
 
 // @route  POST api/items
-// @desc   Create A Post
+// @desc   Create An Item
 // @access Public
 router.post('/', (req, res) => {
     const newItem = new Item({
@@ -25,8 +25,25 @@ router.post('/', (req, res) => {
 
     newItem.save()
         .then(item => {
-            res.json(item);
-        });
+            return res.json(item)
+        })
+        .catch(err => console.error(err));
 });
+
+// @route  DELETE api/items/:id
+// @desc   Delete An Item
+// @access Public
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+        .then(item => {
+            item.remove()
+                .then(() => {
+                    return res.json({success: true})
+                })
+        })
+        .catch(err => {
+            return res.status(404).json({success: false});
+        })
+})
 
 module.exports = router;
