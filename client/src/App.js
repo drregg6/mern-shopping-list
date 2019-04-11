245b1763ce42b40def20fb25e2d7c4fdbcf0c8f8
Dave <drregg6@gmail.com>
2019-04-11 17:36:15 -0400

    implemented Bootstrap and Reactstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import AppNavbar from './components/AppNavbar';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <AppNavbar />
         <h1>Hello world!</h1>
       </div>
     );
