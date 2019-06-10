6ea87076efe6dea0b24843ac2f3ac3c1e06969e7
Dave Regg <drregg6@gmail.com>
2019-06-10 17:55:48 -0400

    EOD 6-10

--- client/src/store.js
+++ client/src/store.js
@@ -9,9 +9,9 @@ const store = createStore(
     rootReducer,
     initState,
     compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(...middleware)
     )
 );
 
 export default store;
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
